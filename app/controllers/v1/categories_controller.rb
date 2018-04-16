55dffd263a16f46eafb67bf6af08f894fd1af1b1
at-duyle <vietdn1991@gmail.com>
2018-04-16 08:19:11 +0700

    Fix serializer category

--- app/controllers/v1/categories_controller.rb
+++ app/controllers/v1/categories_controller.rb
@@ -1,6 +1,6 @@
 class V1::CategoriesController < V1::BaseController
   def index
     render json: CategoryOperations::Index.new(params, actor).call,
-           each_serializer: Categories::IndexSerializer
+           each_serializer: ::Categories::IndexSerializer
   end
 end
